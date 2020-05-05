b272133fa0d5bd30440c89bfd853cc57631938ca
Ramon Chiara <ramonchiara@gmail.com>
2020-05-05 00:26:49 -0300

    Corrigindo o aviso dado pelo GCC mais novo

--- 02-fib.cpp
+++ 02-fib.cpp
@@ -19,7 +19,7 @@ int fastFib(int n, map<int, int> &memo)
 
     try {
         return memo.at(n);
-    } catch(out_of_range) {
+    } catch(out_of_range&) {
         int result = fastFib(n - 1, memo) + fastFib(n - 2, memo);
         memo[n] = result;
         return result;
